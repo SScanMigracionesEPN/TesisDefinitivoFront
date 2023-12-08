617f2a7498f6e67424489958d9ffd63de27b8604
Fernandito <55715279+Ferponce1996@users.noreply.github.com>
2023-12-07 21:55:24 -0500

    ejemplo inicial angular y prime ng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { NavPageComponent } from '@layout';
+import { ActorComponent } from '@pages/strategy/actor/actor.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:NavPageComponent,
+        children:[
+            {
+                path:'actores',
+                component:ActorComponent
+            }
+        ]
+    }
+];
